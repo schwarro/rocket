79caf3a319d2cd6836ff5fb547ca95de942cdbb4
schwarro <rob.ap.schwartz@gmail.com>
2018-08-27 13:08:16 -0400

    added tests

--- rocket_test.rb
+++ rocket_test.rb
@@ -3,4 +3,44 @@ require_relative "rocket"
 
 class RocketTest < Minitest::Test
   # Write your tests here!
+
+  def setup
+    @rocket = Rocket.new
+  end
+
+  def test_name_update
+    expected = 'Apollo'
+    @rocket.name = 'Apollo'
+    actual = @rocket.name
+    assert_equal(expected, actual)
+  end
+
+  def test_color_updater
+    expected = 'blue'
+    @rocket.color = 'blue'
+    actual = @rocket.color
+    assert_equal(expected, actual)
+  end
+
+  def test_flying
+    expected = false
+    @rocket.flying = false
+    actual = @rocket.flying
+    assert_equal(expected, actual)
+  end
+
+  def test_lift_off
+    expected = true
+    @rocket.lift_off
+    actual = @rocket.flying?
+    assert actual
+  end
+
+  def test_landing
+    @rocket.lift_off
+    expected = false
+    @rocket.land
+    actual = @rocket.flying?
+    assert_equal(expected, actual) 
+  end
 end
